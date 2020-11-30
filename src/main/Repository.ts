07b81178a1b8ac5d1f9dd1be43765bb96c8cdc51
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-11-30 09:41:58 +0000

    Refactor error handling, update Repository inf

--- src/main/Repository.ts
+++ src/main/Repository.ts
@@ -1,30 +1,65 @@
+import { FilterQuery } from "mongoose"
+
+export type Filter<T> = FilterQuery<T>
+
+export interface Options<Model extends Object> {
+    limit: number
+    skip: number
+    sort: {
+        [P in keyof Model]: 1 | -1 | "asc" | "desc" | "ascending" | "descending"
+    }
+}
+
 export type WithId<T> = T & { id: string }
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export type MongoObject<T> = T & { _id: string; __v?: any }
 
-export interface Repository<T extends Object, E = never> {
-    getById(id: string): Promise<WithId<T> | null | E>
-    getAll(): Promise<WithId<T>[] | E>
+export interface Repository<T extends Object> {
+    errorHandler: (error: Error) => boolean
+
+    getEstimatedLength(): Promise<number>
+    
+    getEstimatedSize(): Promise<number>
+
+    getExactLength(): Promise<number>
+    getExactLength(filter: Filter<T>): Promise<number>
+
+    getExactSize(): Promise<number>
+    getExactSize(filter: Filter<T>): Promise<number>
+
+    getById(id: string): Promise<WithId<T> | null>
+
+    getAll(): Promise<WithId<T>[]>
+
+    exists(filter: Filter<T>): Promise<boolean>
+
+    firstOrNull(filter: Filter<T>): Promise<WithId<T> | null>
+
+    filter(filter: Filter<T>): Promise<WithId<T>[]>
+    filter(filter: Filter<T>, options: Options<T>): Promise<WithId<T>[]>
+
+    getDistinct(key: (keyof T) & string): Promise<WithId<T>[]>
+    getDistinct(key: (keyof T) & string, options: Options<T>): Promise<WithId<T>[]>
 }
 
-interface InsertableRepository<T extends Object, E = never> extends Repository<T, E> {
-    add(object: T): Promise<WithId<T> | E>
-    insert(object: T): Promise<WithId<T> | E>
-    addObjectWithId(object: WithId<T>): Promise<WithId<T> | E>
-    addWithId(id: string, object: T): Promise<WithId<T> | E>
+interface InsertableRepository<T extends Object> extends Repository<T> {
+    add(object: T): Promise<WithId<T> | null>
+    insert(object: T): Promise<WithId<T> | null>
+    addObjectWithId(object: WithId<T>): Promise<WithId<T> | null>
+    addWithId(id: string, object: T): Promise<WithId<T> | null>
 }
 
-interface UpdateableRepository<T extends Object, E = never> extends Repository<T, E> {
-    update(id: string, object: T): Promise<WithId<T> | null | E>
-    patch(id: string, object: Partial<T>): Promise<WithId<T> | null | E>
+interface UpdateableRepository<T extends Object> extends Repository<T> {
+    update(id: string, object: T): Promise<WithId<T> | null>
+    patch(id: string, object: Partial<T>): Promise<WithId<T> | null>
 }
 
-interface DeleteableRepository<T extends Object, E = never> extends Repository<T, E> {
-    delete(id: string): Promise<WithId<T> | null | E>
-    remove(id: string): Promise<WithId<T> | null | E>
+interface DeleteableRepository<T extends Object> extends Repository<T> {
+    delete(id: string): Promise<WithId<T> | null>
+    remove(id: string): Promise<WithId<T> | null>
 }
 
-export interface MutableRepository<T extends Object, E = never> extends InsertableRepository<T, E>, UpdateableRepository<T, E>, DeleteableRepository<T, E> {
-    readonly: Repository<T, E>
+export interface MutableRepository<T extends Object> extends InsertableRepository<T>, UpdateableRepository<T>, DeleteableRepository<T> {
+    readonly: Repository<T>
 }
