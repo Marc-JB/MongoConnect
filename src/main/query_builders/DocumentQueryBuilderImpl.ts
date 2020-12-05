db41b7f4c63221d1a73d05bfc08f95ba07b10b06
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-12-05 15:04:39 +0000

    Improve QueryBuilder types

--- src/main/query_builders/DocumentQueryBuilderImpl.ts
+++ src/main/query_builders/DocumentQueryBuilderImpl.ts
@@ -1,5 +1,5 @@
 import { Document, DocumentQuery } from "mongoose"
-import { DocumentQueryBuilder, ReplaceValueOfSingleKey, ReplaceValueOfSingleNestedKey } from "./QueryBuilderTypes"
+import { DocumentQueryBuilder, KeyWithStringArrayValue, KeyWithStringValue, ReplaceValueOfSingleKey, ReplaceValueOfSingleNestedArrayKey, ReplaceValueOfSingleNestedKey } from "./QueryBuilderTypes"
 import { convert } from "../convert"
 import { WithId } from "../repositories/Repository"
 
@@ -9,23 +9,27 @@ export class DocumentQueryBuilderImpl<T> implements DocumentQueryBuilder<T> {
         public errorHandler: (error: Error) => boolean = (): boolean => true
     ) {}
 
-    public inlineReferencedObject<R extends Object>(key: keyof T): DocumentQueryBuilder<ReplaceValueOfSingleKey<T, typeof key, WithId<R>>> {
-        type NewDoc = ReplaceValueOfSingleKey<T, typeof key, WithId<R>>
+    public inlineReferencedObject<R extends Object>(key: KeyWithStringValue<T>): DocumentQueryBuilder<ReplaceValueOfSingleKey<T, typeof key, WithId<R> | null>>
+    public inlineReferencedObject<R extends Object>(key: KeyWithStringArrayValue<T>): DocumentQueryBuilder<ReplaceValueOfSingleKey<T, typeof key, WithId<R>[]>>
+    public inlineReferencedObject(key: any): any {
         return new DocumentQueryBuilderImpl(
-            this.query.populate(key) as unknown as DocumentQuery<(Document & NewDoc) | null, Document & NewDoc, {}>,
+            this.query.populate(key),
             this.errorHandler
         )
     }
 
-    public inlineReferencedSubObject<R extends Object>(key: keyof T, subKey: keyof T[typeof key]): DocumentQueryBuilder<ReplaceValueOfSingleNestedKey<T, typeof key, typeof subKey, WithId<R>>> {
-        type NewDoc = ReplaceValueOfSingleNestedKey<T, typeof key, typeof subKey, WithId<R>>
+    public inlineReferencedSubObject<R extends Object>(key: KeyWithStringValue<T>, subKey: KeyWithStringValue<T[typeof key]>): DocumentQueryBuilder<ReplaceValueOfSingleNestedKey<T, typeof key, typeof subKey, WithId<R> | null>>
+    public inlineReferencedSubObject<R extends Object>(key: KeyWithStringArrayValue<T>, subKey: KeyWithStringValue<T[typeof key]>): DocumentQueryBuilder<ReplaceValueOfSingleNestedArrayKey<T, typeof key, typeof subKey, WithId<R> | null>>
+    public inlineReferencedSubObject<R extends Object>(key: KeyWithStringValue<T>, subKey: KeyWithStringArrayValue<T[typeof key]>): DocumentQueryBuilder<ReplaceValueOfSingleNestedKey<T, typeof key, typeof subKey, WithId<R>[]>>
+    public inlineReferencedSubObject<R extends Object>(key: KeyWithStringArrayValue<T>, subKey: KeyWithStringArrayValue<T[typeof key]>): DocumentQueryBuilder<ReplaceValueOfSingleNestedArrayKey<T, typeof key, typeof subKey, WithId<R>[]>>
+    public inlineReferencedSubObject(key: any, subKey: any): any {
         return new DocumentQueryBuilderImpl(
             this.query.populate({
                 path: key,
                 populate: {
                     path: subKey
                 }
-            }) as unknown as DocumentQuery<(Document & NewDoc) | null, Document & NewDoc, {}>,
+            }),
             this.errorHandler
         )
     }
