84785cc589096b4816b3077b410efc7b9586d413
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-12-05 10:27:28 +0000

    Add more docs

--- src/main/repositories/Repository.ts
+++ src/main/repositories/Repository.ts
@@ -29,19 +29,54 @@ export interface Repository<T extends Object> {
     getExactSize(): Promise<number>
     getExactSize(filter: Filter<T>): Promise<number>
 
+    /**
+     * Returns a single model from the database by its id, or null if none
+     * @param id The ID of the model.
+     */
     getById(id: string): Promise<WithId<T> | null>
     queryById(id: string): DocumentQueryBuilder<T>
 
+    /**
+     * Returns all models from the database, or an empty array if none
+     */
     getAll(): Promise<WithId<T>[]>
     queryAll(): DocumentsArrayQueryBuilder<T>
 
+    /**
+     * Returns true if a model matching the filter exists
+     * @param filter The filter
+     */
     exists(filter: Filter<T>): Promise<boolean>
 
+    /**
+     * Returns the first model that matches the filter, or null if none
+     * @param filter The filter
+     */
     firstOrNull(filter: Filter<T>): Promise<WithId<T> | null>
 
+    /**
+     * Returns all models that match the filter, or an empty array if none
+     * @param filter The filter
+     */
     filter(filter: Filter<T>): Promise<WithId<T>[]>
+
+    /**
+     * Returns all models that match the filter, or an empty array if none
+     * @param filter The filter
+     * @param options Additional options
+     */
     filter(filter: Filter<T>, options: Options<T>): Promise<WithId<T>[]>
 
+    /**
+     * Returns all models that have a different value for the specified key
+     * @param key The key for the value that has to be distinct
+     */
     getDistinct(key: (keyof T) & string): Promise<WithId<T>[]>
+
+    /**
+     * Returns all models that have a different value for the specified key
+     * @param key The key for the value that has to be distinct
+     * @param options Additional options
+     */
     getDistinct(key: (keyof T) & string, options: Options<T>): Promise<WithId<T>[]>
 }
